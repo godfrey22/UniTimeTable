63a142e6945578253e16be226a9406b89d3ff2ab
高卓锐 <710110437@qq.com>
2016-04-30 14:29:08 +1000

    Initial Commit

---
+++ UniTimeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniTimeTable
+//
+//  Created by Godfrey Gao on 16/4/30.
+//  Copyright © 2016年 Godfrey Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
