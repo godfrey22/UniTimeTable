6f785741589c88af4f073f2c1d169496e5712a9c
Godfrey Gao <zggao1@student.monash.edu>
2016-05-08 18:54:40 +1000

    Created AddSemester View Controller and associated the file with the storyboard

---
+++ UniTimeTable/AddSemesterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddSemesterViewController.swift
+//  UniTimeTable
+//
+//  Created by Godfrey Gao on 16/5/8.
+//  Copyright © 2016年 Godfrey Gao. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class AddSemesterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
