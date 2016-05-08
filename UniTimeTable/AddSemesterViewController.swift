2e468389fa44cc459e21ec68a581c627497072c6
Godfrey Gao <zggao1@student.monash.edu>
2016-05-08 19:05:41 +1000

    Added 2 date picker works

--- UniTimeTable/AddSemesterViewController.swift
+++ UniTimeTable/AddSemesterViewController.swift
@@ -10,7 +10,58 @@ import UIKit
 import Foundation
 import CoreData
 
+protocol addSemesterDelegate {
+    func addSemester(semester: Semester)
+}
+
 class AddSemesterViewController: UIViewController {
+    
+    var allSemester: NSArray?
+    var delegate: addSemesterDelegate!
+    var managedObjectContext: NSManagedObjectContext
+    
+    @IBOutlet var semesterNameInput: UITextField!
+    @IBOutlet var semesterStartDate: UITextField!
+    @IBOutlet var semesterEndDate: UITextField!
+    
+    //Start Datepicker
+    @IBAction func editStartDate(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddSemesterViewController.startdatePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func startdatePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        //dateInput = sender.date
+        semesterStartDate.text = dateFormatter.stringFromDate(sender.date)
+    }
+    
+    //End Datepicker
+    @IBAction func editEndDate(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddSemesterViewController.enddatePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func enddatePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        //dateInput = sender.date
+        semesterEndDate.text = dateFormatter.stringFromDate(sender.date)
+    }
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
+        super.init(coder: aDecoder)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
