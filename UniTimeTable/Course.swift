e57f3f2d57131697ee6d4376e0ccd0f8851a1eb8
Godfrey Gao <zhuorui.gao@gmail.com>
2016-05-10 15:29:27 +1000

    Initiate Unit Function
    1. Created the data model
    2. Created the view

---
+++ UniTimeTable/Course.swift
@@ -0,0 +1,17 @@
+//
+//  Course.swift
+//  UniTimeTable
+//
+//  Created by Godfrey Gao on 16/5/10.
+//  Copyright © 2016年 Godfrey Gao. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Course: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
