1a7f017e68fb2a899e434ba0308ea10be5c1153a
Godfrey Gao <zhuorui.gao@gmail.com>
2016-05-13 09:23:29 +1000

    Add Class Function Initiation
    1. Add customer cell
    2. config the storyboard

---
+++ UniTimeTable/ClassCell.swift
@@ -0,0 +1,30 @@
+//
+//  ClassCell.swift
+//  UniTimeTable
+//
+//  Created by Godfrey Gao on 16/5/13.
+//  Copyright © 2016年 Godfrey Gao. All rights reserved.
+//
+
+import UIKit
+
+class ClassCell: UITableViewCell {
+
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var teacherLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var weekLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
