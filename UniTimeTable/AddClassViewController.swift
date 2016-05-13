46c5711b4e647b303068d9d6410e0059bc22f112
Godfrey Gao <zhuorui.gao@gmail.com>
2016-05-13 13:10:37 +1000

    Created pickers
    1. Created date pickers and time pickers for the class

--- UniTimeTable/AddClassViewController.swift
+++ UniTimeTable/AddClassViewController.swift
@@ -7,9 +7,69 @@
 //
 
 import UIKit
+import CoreData
 
 class AddClassViewController: UIViewController {
 
+    @IBOutlet var typeInput: UITextField!
+    @IBOutlet var startDateInput: UITextField!
+    @IBOutlet var endDateInput: UITextField!
+    @IBOutlet var startTimeInput: UITextField!
+    @IBOutlet var endTimeInput: UITextField!
+    
+    //Start Datepicker
+    @IBAction func editStartDate(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddClassViewController.startdatePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func startdatePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        startDateInput.text = dateFormatter.stringFromDate(sender.date)
+    }
+    //End Datepicker
+    @IBAction func editEndDate(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddClassViewController.enddatePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func enddatePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        endDateInput.text = dateFormatter.stringFromDate(sender.date)
+    }
+    //Start time input picker
+    @IBAction func editStartTime(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Time
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddClassViewController.startTimePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func startTimePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        startTimeInput.text = dateFormatter.stringFromDate(sender.date)
+    }
+    //End time input picker
+    @IBAction func editEndTime(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Time
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(AddClassViewController.endTimePickerValueChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func endTimePickerValueChanged(sender: UIDatePicker){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        endTimeInput.text = dateFormatter.stringFromDate(sender.date)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
