26bcda51678c9100b0b3026af3ddf467f8aae2e7
Godfrey Gao <zhuorui.gao@gmail.com>
2016-05-15 14:17:38 +1000

    Type Basic Set up
    set up the cell and basic logic for the type controller

--- UniTimeTable/TypeViewController.swift
+++ UniTimeTable/TypeViewController.swift
@@ -14,18 +14,84 @@ class TypeViewController: UIViewController {
     @IBOutlet var typeInput: UITextField!
     @IBOutlet var typeTableView: UITableView!
     
+    var selectedClass: Class!
+    var typeList: NSMutableArray = []
+    var managedObjectContext: NSManagedObjectContext
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.typeList = NSMutableArray()
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
+        super.init(coder: aDecoder)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Register the table view
+        typeTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "TypeCell")
+        
+        //Request all the objects in the "Semester" table
+        let request = NSFetchRequest(entityName: "Type")
+        request.returnsObjectsAsFaults = false
+        
+        //Put all the semester into the semesterList
+        do{
+            let result: NSArray = try managedObjectContext.executeFetchRequest(request)
+            if result.count != 0
+            {
+                self.typeList = NSMutableArray(array: (result as! [Type]))
+            }
+        }catch
+        {
+            let fetchError = error as NSError
+            print(fetchError)
+        }
+        typeTableView.reloadData()
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    //Table View Overrides
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typeList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TypeCellIdentifier", forIndexPath: indexPath) as! TypeCell
+        let t: Type = self.typeList[indexPath.row] as! Type
+        
+        //Config the cell
+        cell.typeLabel.text = t.type_name
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            managedObjectContext.deleteObject(typeList.objectAtIndex(indexPath.row) as! NSManagedObject)
+            self.typeList.removeObjectAtIndex(indexPath.row)
+            typeTableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            self.typeTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .Fade)
+            do
+            {
+                try self.managedObjectContext.save()
+            }
+            catch let error
+            {
+                print("Could not save Deletion \(error)")
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
